ea57a771b829c116a9c375462d4715786c4c153f
poojasharma48 <102306155+poojasharma48@users.noreply.github.com>
2022-05-14 20:34:30 +0530

    Update currentTimeService.php

--- src/currentTimeService.php
+++ src/currentTimeService.php
@@ -13,22 +13,18 @@ class currentTimeService {
 
   protected $language_manager;
   protected $date_formatter;
-
   /**
    * CustomService constructor.
    * @param LanguageDefault $language_manager
    * @param DateFormatter date_formatter
   */ 
-
   public function __construct(LanguageDefault $language_manager, DateFormatter $date_formatter) {
     $this->language_manager = $language_manager;
     $this->date_formatter = $date_formatter;
   } 
-
   /**
    * @return \Drupal\Component\Render\MarkupInterface|string
   */
-
   public function getClockCurrentTime($type, $format = '', $location_timezone) {
     // Get current language code.
     $lang = $this->language_manager->get();
@@ -36,8 +32,6 @@ class currentTimeService {
     /*Get the current date*/
     $date_formatter = $this->date_formatter;
     $formatted_date = $date_formatter->format(time(), $type, $format, $location_timezone, $langcode);
-
     return $formatted_date;
   }
-
 }
